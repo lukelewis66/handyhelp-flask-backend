93341d739bcb25ac56c2788c78491b8874ebde8f
Luke Lewis <lukelewis@ucsb.edu>
2020-11-10 21:01:51 +0000

    ll - changed environ to os.environ

--- myapp.py
+++ myapp.py
@@ -85,15 +85,15 @@ def upload():
 # cred = credentials.Certificate("handyhelp-f4192-firebase-adminsdk-hgsp6-cbe87ca6a8.json")
 ENV_KEYS = {
     "type": "service_account",
-    "project_id": environ['FIREBASE_PROJECT_ID'],
-    "private_key_id": environ['FIREBASE_PRIVATE_KEY_ID'],
-    "private_key": environ['FIREBASE_PRIVATE_KEY'].replace("\\n", "\n"),
-    "client_email": environ['FIREBASE_CLIENT_EMAIL'],
-    "client_id": environ['FIREBASE_CLIENT_ID'],
-    "auth_uri": environ['FIREBASE_AUTH_URI'],
-    "token_uri": environ['FIREBASE_TOKEN_URI'],
-    "auth_provider_x509_cert_url": environ["FIREBASE_AUTH_PROVIDER_X509_CERT_URL"],
-    "client_x509_cert_url": environ['FIREBASE_CLIENT_X509_CERT_URL'],
+    "project_id": os.environ['FIREBASE_PROJECT_ID'],
+    "private_key_id": os.environ['FIREBASE_PRIVATE_KEY_ID'],
+    "private_key": os.environ['FIREBASE_PRIVATE_KEY'].replace("\\n", "\n"),
+    "client_email": os.environ['FIREBASE_CLIENT_EMAIL'],
+    "client_id": os.environ['FIREBASE_CLIENT_ID'],
+    "auth_uri": os.environ['FIREBASE_AUTH_URI'],
+    "token_uri": os.environ['FIREBASE_TOKEN_URI'],
+    "auth_provider_x509_cert_url": os.environ["FIREBASE_AUTH_PROVIDER_X509_CERT_URL"],
+    "client_x509_cert_url": os.environ['FIREBASE_CLIENT_X509_CERT_URL'],
 }
 cred = credentials.Certificate(ENV_KEYS)
 firebase_admin.initialize_app(cred)
