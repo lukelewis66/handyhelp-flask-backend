bef784ef567b3b68cfedc77e37a004e2edfb134e
Luke Lewis <lukelewis@ucsb.edu>
2020-11-10 20:36:55 +0000

    ll - comment out auth url for firebase credentials

--- myapp.py
+++ myapp.py
@@ -92,8 +92,8 @@ cred = credentials.Certificate({
     "client_id": os.getenv('FIREBASE_CLIENT_ID'),
     "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
     "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_X509_CERT_URL'),
+    # "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    # "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_X509_CERT_URL'),
 })
 firebase_admin.initialize_app(cred)
 db = firestore.client()
