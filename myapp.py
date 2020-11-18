29fa2b4b4510c8f82ea6949b54b299348294b4e9
henryanderson64 <hankanderson135790@gmail.com>
2020-11-18 22:20:24 +0000

    merge

--- myapp.py
+++ myapp.py
@@ -2,7 +2,9 @@
 
 
 from math import sin, cos, sqrt, atan2, radians
-import os, json, boto3
+import os
+import json
+import boto3
 from botocore.config import Config
 from os.path import join, dirname
 import datetime
@@ -13,16 +15,16 @@ from FirebaseHelpers import getDictFromList
 from dotenv import load_dotenv
 load_dotenv(override=True)
 
-#use this if linking to a reaact app on the same server
+# use this if linking to a reaact app on the same server
 app = Flask(__name__, static_folder='./build', static_url_path='/')
-DEBUG=True
+DEBUG = True
 
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 
-### CORS section
+# CORS section
 @app.after_request
 def after_request_func(response):
     if DEBUG:
@@ -34,7 +36,7 @@ def after_request_func(response):
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
         response.headers.add('Access-Control-Allow-Headers', 'x-csrf-token')
         response.headers.add('Access-Control-Allow-Methods',
-                            'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+                             'GET, POST, OPTIONS, PUT, PATCH, DELETE')
         if origin:
             response.headers.add('Access-Control-Allow-Origin', origin)
     else:
@@ -43,19 +45,20 @@ def after_request_func(response):
             response.headers.add('Access-Control-Allow-Origin', origin)
 
     return response
-### end CORS section
+# end CORS section
 
 
-
-### boto3 session for any S3 functions
+# boto3 session for any S3 functions
 session = boto3.Session(
-            aws_access_key_id=os.getenv('ACCESS_KEY'),
-            aws_secret_access_key=os.getenv('SECRET_KEY'),
-            region_name=os.getenv('REGION_NAME'),
-        )
+    aws_access_key_id=os.getenv('ACCESS_KEY'),
+    aws_secret_access_key=os.getenv('SECRET_KEY'),
+    region_name=os.getenv('REGION_NAME'),
+)
 s3 = session.resource('s3')
 
-### initializes an empty bucket named after the given UID
+# initializes an empty bucket named after the given UID
+
+
 @app.route("/bucketinit", methods=['POST', 'GET'])
 def bucketinit():
     requestUID = request.form['Bucket']
@@ -64,12 +67,14 @@ def bucketinit():
         ACL=f'{requestACL}',
         Bucket=f'{requestUID.lower()}',
         CreateBucketConfiguration={
-          'LocationConstraint' : 'us-west-1'  
+            'LocationConstraint': 'us-west-1'
         },
     )
     return ''
 
-### uploads given image to the bucket named by the UID
+# uploads given image to the bucket named by the UID
+
+
 @app.route("/upload", methods=['POST', 'GET'])
 def upload():
     uploaded_file = request.files.get('file')
@@ -79,8 +84,10 @@ def upload():
         key = 'ProfilePic.png'
     else:
         key = 'Listings/' + request.form['type'] + '/' + uploaded_file.filename
-    s3.Bucket(UID.lower()).put_object(ACL='public-read-write', Key=f'{key}', Body=uploaded_file)
+    s3.Bucket(UID.lower()).put_object(
+        ACL='public-read-write', Key=f'{key}', Body=uploaded_file)
     return ''
+
 
 # cred = credentials.Certificate("handyhelp-f4192-firebase-adminsdk-hgsp6-cbe87ca6a8.json")
 ENV_KEYS = {
@@ -102,14 +109,17 @@ db = firestore.client()
 # ----------------------------------------------------------------------------------------------------------------
 #   ACCOUNT
 # ----------------------------------------------------------------------------------------------------------------
+
+
 @app.route('/checkuserexists', methods=['GET'])
 def checkuserexist():
     if "UID" in request.args:
         UID = request.args.get("UID")
         account_ref = db.collection('users').document(UID).get()
-        return {"exists" : account_ref.exists}, 200
+        return {"exists": account_ref.exists}, 200
     else:
         return "error", 400
+
 
 @app.route('/createaccount', methods=['POST'])
 def createaccount():
@@ -136,15 +146,17 @@ def createaccount():
         new_contractor_ref.set(contractor_data)
     return "success", 200
 
+
 @app.route('/checkuseractive', methods=['GET'])
 def checkuseractive():
     if "UID" in request.args:
         UID = request.args.get("UID")
         user_ref = db.collection('users').document(UID).get()
         user = user_ref.to_dict()
-        return {"active" : user["active"]}, 200
+        return {"active": user["active"]}, 200
     else:
         return "error", 400
+
 
 @app.route('/deactivateaccount', methods=['POST'])
 def deactivateaccount():
@@ -152,8 +164,9 @@ def deactivateaccount():
     # body = json.loads(request.data)
     # UID = body["UID"]
     user_ref = db.collection('users').document(UID)
-    user_ref.update({ 'active' : False })
+    user_ref.update({'active': False})
     return "success", 200
+
 
 @app.route('/reactivateaccount', methods=['POST'])
 def reactivateaccount():
@@ -161,18 +174,21 @@ def reactivateaccount():
     # body = json.loads(request.data)
     # UID = body["UID"]
     user_ref = db.collection('users').document(UID)
-    user_ref.update({ 'active' : True })
+    user_ref.update({'active': True})
     return "success", 200
 
 # ----------------------------------------------------------------------------------------------------------------
 #   USER
 # ----------------------------------------------------------------------------------------------------------------
+
+
 @app.route('/getusers/', methods=['GET'])
 def getusers():
     result = db.collection('users').get()
     records = getDictFromList(result)
     print(records)
     return jsonify(records), 200
+
 
 @app.route('/getuser', methods=['GET'])
 def getuser():
@@ -182,6 +198,7 @@ def getuser():
     userDict = user.to_dict()
     return jsonify(userDict), 200
 
+
 @app.route('/editInfo/', methods=['POST'])
 def editInfo():
     UID = request.form['UID']
@@ -190,12 +207,14 @@ def editInfo():
     # body = json.loads(request.data)
     # UID = body["UID"]
     user_ref = db.collection('users').document(UID)
-    user_ref.update({ 'name' : NAME })
-    user_ref.update({ 'phone' : PHONE })
+    user_ref.update({'name': NAME})
+    user_ref.update({'phone': PHONE})
     return "success", 200
 # ----------------------------------------------------------------------------------------------------------------
 #   LISTING
 # ----------------------------------------------------------------------------------------------------------------
+
+
 @app.route('/addlisting/', methods=['POST'])
 def addlisting():
     body = json.loads(request.data)
@@ -208,7 +227,8 @@ def addlisting():
         u'skilltags': body["skilltags"],
         u'date_posted': datetime.datetime.now(),
     }
-    new_listing_ref = db.collection(u'listings').document() #get the auto generated document id
+    # get the auto generated document id
+    new_listing_ref = db.collection(u'listings').document()
     new_listing_ref.set(data)
     return new_listing_ref.id, 200
 
@@ -218,15 +238,15 @@ def addlisting():
 #     UID = body["UID"]
 #     result = db.collection('listings').get()
 #     records = getDictFromList(result)
-    
 
 
 @app.route('/getlistings', methods=['GET'])
 def getlistings():
     result = db.collection('listings').get()
     records = getDictFromList(result)
-    #test for gitignore
+    # test for gitignore
     return jsonify(records), 200
+
 
 @app.route('/updatelistingimages', methods=['POST'])
 def updatelistingimages():
@@ -238,9 +258,6 @@ def updatelistingimages():
         "images": imageUrls
     }, merge=True)
     return "success", 200
-
-
-
 
 
 # ----------------------------------------------------------------------------------------------------------------
@@ -258,11 +275,14 @@ def getallcontractors():
     allUsers = db.collection('users').get()
     allUsersDict = getDictFromList(allUsers)
     print("ALl users dict before: ", allUsersDict)
-    delete = [key for key in allUsersDict if allUsersDict[key]['role'] != "contractor"]
-    for key in delete: del allUsersDict[key]
+    delete = [key for key in allUsersDict if allUsersDict[key]
+              ['role'] != "contractor"]
+    for key in delete:
+        del allUsersDict[key]
     print(allUsersDict)
     for key in allUsersDict:
-        contractor_ref = db.collection('contractors').document(allUsersDict[key]['id']).get()
+        contractor_ref = db.collection('contractors').document(
+            allUsersDict[key]['id']).get()
         # contractor_ref = db.collection('contractors').document(user['id']).get()
         allUsersDict[key].update(contractor_ref.to_dict())
     return jsonify(allUsersDict)
@@ -273,9 +293,11 @@ def addcontractor():
     try:
         body = json.loads(request.data)
         data = {
-            u'name'     : body["name"],
-            u'email'    : body["email"],
-            u'password' : body["password"],
+            u'name': body["name"],
+            u'email': body["email"],
+            u'password': body["password"],
+            u'averageReview': 0,
+            u'reviewCount' : 0,
         }
         newClient = db.collection(u'contractors').document()
         newClient.set(data)
@@ -290,6 +312,7 @@ def getreviews():
     records = getDictFromList(result)
     return jsonify(records), 200
 
+
 @app.route('/isClient', methods=['GET'])
 def isCLient():
     UID = request.args.get("UID")
@@ -297,15 +320,17 @@ def isCLient():
     records = getDictFromList(result)
     return jsonify(records), 200
 
+
 @app.route('/getlisting', methods=['GET'])
 def getlisting():
     LID = request.args.get("LID")
     print(LID)
     result = db.collection('listings').document(LID).get()
     print(type(result.to_dict()))
-    #test for gitignore
+    # test for gitignore
 
     return jsonify(result.to_dict()), 200
+
 
 @app.route('/getcontractor', methods=['GET'])
 def getcontractor():
@@ -316,9 +341,10 @@ def getcontractor():
     userDict = user.to_dict()
     userDict.update(contractor.to_dict())
     #result = db.collection('contractors').document(UID).get()
-    #test for gitignore
+    # test for gitignore
 
     return jsonify(userDict), 200
+
 
 @app.route('/editContractor/', methods=['POST'])
 def editContractor():
@@ -326,11 +352,12 @@ def editContractor():
     UID = body["UID"]
     user_ref = db.collection('users').document(UID)
     contractor_ref = db.collection('contractors').document(UID)
-    #db.contractor_ref.update({UID}, {$set: {'name': data.name}})
-    user_ref.update({'name' : body["name"]})
-    user_ref.update({'phone' : body["phone"]})
-    contractor_ref.update({'skilltags' : body["skilltags"]})
-    contractor_ref.update({'bio' : body["bio"]})
+    # db.contractor_ref.update({UID}, {$set: {'name': data.name}})
+    user_ref.update({'name': body["name"]})
+    user_ref.update({'phone': body["phone"]})
+    contractor_ref.update({'skilltags': body["skilltags"]})
+    contractor_ref.update({'bio': body["bio"]})
+
 
 @app.route('/updateprofilepicture', methods=['POST'])
 def updateprofilepicture():
@@ -355,21 +382,22 @@ def getrole():
     print("UID :" + UID)
     result = db.collection('users').document(UID).get()
     print(result.to_dict())
-    #test for gitignore
+    # test for gitignore
 
     return jsonify(result.to_dict()), 200
+
 
 @app.route('/getcontracts', methods=['GET'])
 def getcontracts():
     result = db.collection('contracts').get()
-    records = getDictFromList(result)   
+    records = getDictFromList(result)
     return jsonify(records), 200
 
 
 # @app.route('/getdistance', methods=['GET'])
 # def getdistance():
-    
-    
+
+
 #     try:
 #         UID1 = request.args.get("UID1")
 #         UID2 = request.args.get("UID2")
@@ -382,7 +410,7 @@ def getcontracts():
 #         lat2 = other.location[1]
 #     except:
 #         return jsonify(-1), 200
-    
+
 #     origin = new google.maps.LatLng(lat1, lon1);
 #     destination = new google.maps.LatLng(lat2, lon2);
 
@@ -403,25 +431,18 @@ def getcontracts():
 #   print("geoDistance response:" + respose + " | status: " + status)
 # }
 
-    
 
 #     print("[" + lat1 + "," + lon1 + "] => [" + lat2 + "," + lon2 + "]")
-    
+
 
 #     print("\n\nMiles:", distance)
 
 #     return {"distance" : distance}, 200
 
 
-
-
-
-
-
-
 @app.route('/api/getmsg/', methods=['GET'])
 def respond():
-    # Retrieve the msg from url parameter of GET request 
+    # Retrieve the msg from url parameter of GET request
     # and return MESSAGE response (or error or success)
     msg = request.args.get("msg", None)
 
@@ -429,52 +450,58 @@ def respond():
         print("GET respond() msg: {}".format(msg))
 
     response = {}
-    if not msg: #invalid/missing message
+    if not msg:  # invalid/missing message
         response["MESSAGE"] = "no msg key found, please send a msg."
         status = 400
-    else: #valid message
+    else:  # valid message
         response["MESSAGE"] = f"Welcome {msg}!"
         status = 200
 
     # Return the response in json format with status code
     return jsonify(response), status
 
-@app.route('/api/keys/', methods=['POST']) 
-def postit(): 
-    
+
+@app.route('/api/keys/', methods=['POST'])
+def postit():
+
     response = {}
-    #only accept json content type
+    # only accept json content type
     if request.headers['content-type'] != 'application/json':
-        return jsonify({"MESSAGE": "invalid content-type"}),400
+        return jsonify({"MESSAGE": "invalid content-type"}), 400
     else:
         try:
             data = json.loads(request.data)
         except ValueError:
-            return jsonify({"MESSAGE": "JSON load error"}),405
+            return jsonify({"MESSAGE": "JSON load error"}), 405
     acc = data['acckey']
     sec = data['seckey']
     if DEBUG:
-        print("POST: acc={}, sec={}".format(acc,sec))
+        print("POST: acc={}, sec={}".format(acc, sec))
     if acc:
-        response["MESSAGE"]= "Welcome! POST args are {} and {}".format(acc,sec)
+        response["MESSAGE"] = "Welcome! POST args are {} and {}".format(
+            acc, sec)
         status = 200
     else:
-        response["MESSAGE"]= "No acckey or seckey keys found, please resend."
+        response["MESSAGE"] = "No acckey or seckey keys found, please resend."
         status = 400
 
     return jsonify(response), status
 
 # Set the base route to be the react index.html
+
+
 @app.route('/')
 def index():
-    
+
     return "hello!"
+
 
 def main():
     '''The threaded option for concurrent accesses, 0.0.0.0 host says listen to all network interfaces (leaving this off changes this to local (same host) only access, port is the port listened on -- this must be open in your firewall or mapped out if within a Docker container. In Heroku, the heroku runtime sets this value via the PORT environment variable (you are not allowed to hard code it) so set it from this variable and give a default value (8118) for when we execute locally.  Python will tell us if the port is in use.  Start by using a value > 8000 as these are likely to be available.
     '''
     localport = int(os.getenv("PORT", 8118))
     app.run(threaded=True, host='0.0.0.0', port=localport)
+
 
 if __name__ == '__main__':
     main()
